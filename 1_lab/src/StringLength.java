144df5c937ce74e3f682746f909e3f2c117d9f40
Buyanov Vladimir <rawen17@mail.ru>
2021-02-28 08:14:02 +0300

    1_lab

--- 1_lab/src/StringLength.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class StringLength implements ITask {
-    @Override
-    public String Run(ArrayList<String> data) {
-        return String.valueOf(data.get(0).length());
-    }
-}
